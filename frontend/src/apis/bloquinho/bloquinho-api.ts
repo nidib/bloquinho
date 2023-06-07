30ccca90faa33a7cf274d9ec6e5e94cd3ca570ec
Richard Bidin <richardbidin@outlook.com>
2023-06-07 00:49:53 -0300

    Utiliza env correta

--- frontend/src/apis/bloquinho/bloquinho-api.ts
+++ frontend/src/apis/bloquinho/bloquinho-api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 export const bloquinhoApi = axios.create({
-	baseURL: import.meta.env.API_BASE_URL ?? 'http://localhost:8080/api',
+	baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api',
 });
 
 export type CreatedBloquinho = {
