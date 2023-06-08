28fca4bcad457649e2235c56e9455267284aede4
Richard Bidin <richardbidin@outlook.com>
2023-06-08 00:06:30 -0300

    simplifica retorno

--- backend/src/domains/bloquinho/usecases/check-if-bloquinho-exists-by-title-usecase.ts
+++ backend/src/domains/bloquinho/usecases/check-if-bloquinho-exists-by-title-usecase.ts
@@ -3,9 +3,7 @@ import { BloquinhoRepository } from '../bloquinho-repository';
 export class CheckIfBloquinhoExistsByTitleUsecase {
 
 	public static async execute(title: string): Promise<boolean> {
-		const existingBloquinho = await BloquinhoRepository.getBloquinhoByTitle(title);
-
-		return Boolean(existingBloquinho);
+		return Boolean(await BloquinhoRepository.getBloquinhoByTitle(title));
 	}
 
 }
