e8561588ec4891bb1d7a9b1ca53c19e5fff510eb
Richard Bidin <richardbidin@outlook.com>
2023-09-07 21:24:18 -0300

    Backend: Removes content length database constraint

--- backend/src/domains/bloquinho/bloquinho-model.ts
+++ backend/src/domains/bloquinho/bloquinho-model.ts
@@ -8,7 +8,7 @@ export const supportedExtensions = ['java', 'js', 'jsx', 'ts', 'tsx', 'txt', 'sq
 export const bloquinhoModel = schema.table('bloquinho', {
 	...baseColumns,
 	title: varchar('title', { length: 50 }).notNull(),
-	content: varchar('content', { length: 1000 }).notNull(),
+	content: varchar('content').notNull(),
 	isPublic: boolean('is_public').notNull().default(true),
 	extension: varchar('extension', { length: 10, enum: supportedExtensions }).notNull().default('txt'),
 	lastViewedAt: timestamp('last_viewed_at'),
