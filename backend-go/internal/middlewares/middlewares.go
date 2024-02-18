62abd50a9df07706f841f167ee5bfcbda9ed3d68
Richard Bidin <richardbidin@outlook.com>
2024-02-17 23:29:56 -0300

    Modo recover + logs + 404

---
+++ backend-go/internal/middlewares/middlewares.go
@@ -0,0 +1,26 @@
+package middlewares
+
+import (
+	"time"
+
+	appLogger "github.com/nidib/bloquinho/backend/internal/logger"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+func Recover() func(*fiber.Ctx) error {
+	return recover.New()
+}
+
+func Logger() func(*fiber.Ctx) error {
+	format := "time=${time} method=${method} path=${path} queryParams=${queryParams} status=${status} latency=${latency} bytesReceived=${bytesReceived} ip=${ip} bytesSent=${bytesSent} error=${error} \n"
+
+	return logger.New(logger.Config{
+		TimeFormat:    time.RFC3339,
+		DisableColors: true,
+		Format:        format,
+		Output:        *appLogger.GetLoggerHandler(),
+	})
+}
