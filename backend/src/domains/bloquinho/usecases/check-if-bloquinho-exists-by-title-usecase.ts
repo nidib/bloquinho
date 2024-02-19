c90a27b246421c76b8c9ae946decddaf9412f371
Richard Bidin <richardbidin@outlook.com>
2024-02-18 22:35:24 -0300

    Remove backend em node

--- backend/src/domains/bloquinho/usecases/check-if-bloquinho-exists-by-title-usecase.ts
+++
@@ -1,7 +0,0 @@
-import { BloquinhoRepository } from '../bloquinho-repository';
-
-export class CheckIfBloquinhoExistsByTitleUsecase {
-	public static async execute(title: string): Promise<boolean> {
-		return Boolean(await BloquinhoRepository.getBloquinhoByTitle(title));
-	}
-}
--- backend/src/domains/bloquinho/usecases/retrieve-bloquinho-by-title-usecase.ts
+++
@@ -1,9 +0,0 @@
-import { Optional } from '../../../utils/types';
-import { BloquinhoModel } from '../bloquinho-model';
-import { BloquinhoRepository } from '../bloquinho-repository';
-
-export class RetrieveBloquinhoByTitle {
-	public static async execute(title: string): Promise<Optional<BloquinhoModel>> {
-		return BloquinhoRepository.getBloquinhoByTitle(title);
-	}
-}
