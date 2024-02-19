5cab80da4fe6dd0bf1707b859dd5413a9a8e2152
Richard Bidin <richardbidin@outlook.com>
2024-02-19 18:33:02 -0300

    Cria pagina de manutencao

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,25 +1,37 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import { BloquinhoEditorPage } from './pages/bloquinho-editor-page';
 import { HomePage } from './pages/home-page';
-
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <HomePage />,
-	},
-	{
-		path: '/:title',
-		element: <BloquinhoEditorPage />,
-	},
-	{
-		path: '*',
-		element: <div>not found</div>,
-	},
-]);
+import { MaintenancePage } from './pages/mainetance';
 
 function App() {
+	const routes = getRoutes();
+	const router = createBrowserRouter(routes);
+
 	return <RouterProvider router={router} />;
+}
+
+function getRoutes(): RouteObject[] {
+	const isUnderMaintenance = import.meta.env.MAINTENANCE ?? false;
+
+	if (isUnderMaintenance) {
+		return [{ path: '*', element: <MaintenancePage /> }];
+	}
+
+	return [
+		{
+			path: '/',
+			element: <HomePage />,
+		},
+		{
+			path: '/:title',
+			element: <BloquinhoEditorPage />,
+		},
+		{
+			path: '*',
+			element: <div>not found</div>,
+		},
+	];
 }
 
 export default App;
--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
 	readonly VITE_API_URL?: string;
+	readonly MAINTENANCE?: boolean;
 }
 
 interface ImportMeta {
