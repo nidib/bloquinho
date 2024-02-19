5d5b8017d0ea0db842f8f1d1f1339a65aa4995c9
Richard Bidin <richardbidin@outlook.com>
2024-02-18 22:15:39 -0300

    Corrige api url local do frontend

--- frontend/src/apis/bloquinho/bloquinho-api.ts
+++ frontend/src/apis/bloquinho/bloquinho-api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { Extension } from '../../utils/constants/extensions';
 
 export const bloquinhoApi = axios.create({
-	baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/api',
+	baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:8080/',
 });
 
 export type PersistedBloquinho = {
