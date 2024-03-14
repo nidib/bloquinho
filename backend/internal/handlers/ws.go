c1d67230f01f854889bf7fa1b7b52e9ca880b020
Richard Bidin <richardbidin@outlook.com>
2024-03-13 23:30:59 -0300

    Implementacao inicial de coordenadas no websocket

--- backend/internal/handlers/ws.go
+++ backend/internal/handlers/ws.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"sync"
 
 	"github.com/gofiber/contrib/websocket"
 	"github.com/gofiber/fiber/v2"
@@ -15,6 +17,39 @@ func WsRequestUpgrade(c *fiber.Ctx) error {
 	return fiber.ErrUpgradeRequired
 }
 
+type Coord struct {
+	X uint16
+	Y uint16
+}
+
+type Client struct {
+	conn  *websocket.Conn
+	id    string
+	coord Coord
+}
+
+type WSReq struct {
+	Coord struct {
+		X uint16 `json:"x"`
+		Y uint16 `json:"y"`
+	}
+}
+
+type WSResCoord struct {
+	X uint16 `json:"x"`
+	Y uint16 `json:"y"`
+}
+
+type WSRes struct {
+	Id    string     `json:"id"`
+	Coord WSResCoord `json:"coord"`
+}
+
+var (
+	rooms     = make(map[string]map[string]*Client)
+	roomsLock sync.Mutex
+)
+
 func WsCoord(conn *websocket.Conn) {
 	var messageType int
 	var message []byte
@@ -22,18 +57,81 @@ func WsCoord(conn *websocket.Conn) {
 
 	room := conn.Params("room", "")
 	clientId := conn.Params("clientId", "")
-	fmt.Printf("Connected client %s at room %s", clientId, room)
-	defer fmt.Printf("Disconnected client %s at room %s", clientId, room)
+	addClient(room, clientId, conn)
+	fmt.Println("Connected client:", clientId)
+	defer removeClient(room, clientId)
 
 	for {
 		if messageType, message, err = conn.ReadMessage(); err != nil {
 			fmt.Println("read error:", err)
 			break
 		}
+		req := WSReq{}
+		json.Unmarshal(message, &req)
 
-		if err = conn.WriteMessage(messageType, message); err != nil {
-			fmt.Println("write error:", err)
+		updateClient(room, clientId, Coord{X: req.Coord.X, Y: req.Coord.Y})
+		clientsInRoom := getClientsByRoom(room)
+		var presentableClients []WSRes
+		for _, clientInRoom := range clientsInRoom {
+			presentableClients = append(presentableClients, WSRes{
+				Id: clientInRoom.id,
+				Coord: WSResCoord{
+					X: clientInRoom.coord.X,
+					Y: clientInRoom.coord.Y,
+				},
+			})
+		}
+		output, err := json.Marshal(presentableClients)
+		if err != nil {
+			fmt.Println("marshal error:", err)
 			break
 		}
+
+		for _, client := range rooms[room] {
+			if err = client.conn.WriteMessage(messageType, output); err != nil {
+				fmt.Println("write:", err)
+				break
+			}
+		}
 	}
+}
+
+func addClient(room string, clientId string, conn *websocket.Conn) string {
+	roomsLock.Lock()
+	defer roomsLock.Unlock()
+	addedClient := &Client{conn: conn, id: clientId}
+
+	if rooms[room] == nil {
+		rooms[room] = make(map[string]*Client)
+	}
+	rooms[room][clientId] = addedClient
+
+	return clientId
+}
+
+func getClientsByRoom(room string) []*Client {
+	roomsLock.Lock()
+	defer roomsLock.Unlock()
+	var clients []*Client
+
+	for _, client := range rooms[room] {
+		clients = append(clients, client)
+	}
+
+	return clients
+}
+
+func updateClient(room string, clientId string, coord Coord) {
+	roomsLock.Lock()
+	defer roomsLock.Unlock()
+	rooms[room][clientId].coord = Coord{
+		X: coord.X,
+		Y: coord.Y,
+	}
+}
+
+func removeClient(room string, clientId string) {
+	roomsLock.Lock()
+	defer roomsLock.Unlock()
+	delete(rooms[room], clientId)
 }
