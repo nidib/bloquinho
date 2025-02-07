4101f5184c64e92e39ac43a9163c7a988656a663
Richard Bidin <richardbidin@outlook.com>
2025-02-07 08:26:20 -0300

    removes extra space

--- app/src/components/maintenance.tsx
+++ app/src/components/maintenance.tsx
@@ -1,7 +1,7 @@
 export function MaintenancePage() {
 	return (
 		<div className="h-screen w-full flex flex-col gap-3 items-center justify-center text-zinc-600">
-			<h1 className="text-2xl lg:text-4xl font-bold ">
+			<h1 className="text-2xl lg:text-4xl font-bold">
 				<span className="font-extrabold">Bloquinho</span> em manutenção
 			</h1>
 			<h2 className="text-lg lg:text-2xl">Tente novamente mais tarde.</h2>
