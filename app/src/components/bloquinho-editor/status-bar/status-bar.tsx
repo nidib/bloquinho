91688026b96358f97f5b6ec144cbb7c4875bec89
Richard Bidin <richardbidin@outlook.com>
2025-02-08 16:09:24 -0300

    feat(app): replaces check box with drop down menu

--- app/src/components/bloquinho-editor/status-bar/status-bar.tsx
+++ app/src/components/bloquinho-editor/status-bar/status-bar.tsx
@@ -1,8 +1,17 @@
 'use client';
 
+import { SlidersVerticalIcon } from 'lucide-react';
 import { ExtensionsSelect } from 'src/components/bloquinho-editor/status-bar/extensions-select';
 import { StatusIndicator } from 'src/components/bloquinho-editor/status-bar/status-indicator';
-import { Checkbox } from 'src/components/form/checkbox';
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from 'src/components/drop-down-menu';
 import type { Extension } from 'src/lib/types/bloquinho';
 import { cn } from 'src/utils/classes';
 
@@ -27,16 +36,23 @@ export function StatusBar(props: Props) {
 
 	return (
 		<footer className="font-mono border-t border-t-zinc-200 py-2 px-3 shrink-0 flex items-center justify-end gap-4">
-			<div className="flex items-center justify-start gap-2">
-				<label htmlFor="line-wrap" className="cursor-pointer text-xs">
-					Quebra de linha:
-				</label>
-				<Checkbox
-					id="line-wrap"
-					value={props.lineWrap}
-					onChange={props.onLineWrapChange}
-				/>
-			</div>
+			<DropdownMenu>
+				<DropdownMenuTrigger>
+					<SlidersVerticalIcon className="w-4 h-4" />
+				</DropdownMenuTrigger>
+				<DropdownMenuContent className="w-56">
+					<DropdownMenuLabel>Preferências</DropdownMenuLabel>
+					<DropdownMenuSeparator />
+					<DropdownMenuGroup>
+						<DropdownMenuCheckboxItem
+							checked={props.lineWrap}
+							onCheckedChange={props.onLineWrapChange}
+						>
+							<span>Quebra de linha</span>
+						</DropdownMenuCheckboxItem>
+					</DropdownMenuGroup>
+				</DropdownMenuContent>
+			</DropdownMenu>
 			<Separator />
 			<ExtensionsSelect
 				value={props.extension}
