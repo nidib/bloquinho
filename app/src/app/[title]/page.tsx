2253f3b2f662f692ba257f9203bad85238294880
Richard Bidin <richardbidin@outlook.com>
2025-02-07 22:47:42 -0300

    app: rendering the bloquinho by title

--- app/src/app/[title]/page.tsx
+++ app/src/app/[title]/page.tsx
@@ -1,3 +1,5 @@
+import { mongoCollections } from 'src/lib/infra/mongo';
+
 type Props = {
 	params: Promise<{
 		title: string;
@@ -6,6 +8,16 @@ type Props = {
 
 export default async function BloquinhoPage({ params }: Props) {
 	const title = (await params).title;
+	const bloquinho = await mongoCollections.bloquinho.findOne({ title });
 
-	return <h1>Título: {title}</h1>;
+	if (!bloquinho) {
+		return null;
+	}
+
+	return (
+		<ul>
+			<li>Title: {bloquinho.title}</li>
+			<li>Content: {bloquinho.content}</li>
+		</ul>
+	);
 }
