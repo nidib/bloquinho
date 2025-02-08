dc420522f6e230e130062777da94a1e8eb641e5e
Richard Bidin <richardbidin@outlook.com>
2025-02-08 14:03:34 -0300

    feat(app): api response on server sanitized

--- app/src/app/[title]/api/route.ts
+++ app/src/app/[title]/api/route.ts
@@ -1,8 +1,8 @@
 import { updateBloquinhoByTitle } from 'src/lib/infra/mongo/services';
 import { extensions } from 'src/lib/types/bloquinho';
 import { z } from 'zod';
-
-const bodySchema = z.object({
+const responseSchema = z.object({
+	title: z.string(),
 	content: z.string(),
 	extension: z.enum(extensions),
 });
@@ -13,11 +13,12 @@ type Options = {
 
 export async function PUT(request: Request, options: Options) {
 	const params = await options.params;
-	const body = bodySchema.parse(await request.json());
+	const body = await request.json();
 
 	const updatedBloquinho = await updateBloquinhoByTitle(params.title, body);
+	const responseBody = responseSchema.parse(updatedBloquinho);
 
-	return Response.json(updatedBloquinho, {
+	return Response.json(responseBody, {
 		status: 200,
 	});
 }
