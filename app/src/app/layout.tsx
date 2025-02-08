109d9ba8ebb8365e0d2a25e09e80d660ef215395
Richard Bidin <richardbidin@outlook.com>
2025-02-07 22:46:10 -0300

    app: reorganizes the css import

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
-import './globals.css';
 import { App } from 'src/utils/constants/app-constants';
+import './globals.css';
 
 const nunito = Nunito({
 	variable: '--font-nunito',
