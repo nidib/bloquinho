3a4f5af9cbf3688d9675122cfbd686617957861f
Richard Bidin <richardbidin@outlook.com>
2025-02-08 13:35:08 -0300

    feat(app): util for async debouncing

---
+++ app/src/utils/functions.ts
@@ -0,0 +1,19 @@
+import debounce from 'lodash/debounce';
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export function asyncDebounce<T extends (...args: any) => any>(
+	func: T,
+	wait: number,
+): (...args: Parameters<T>) => Promise<Awaited<ReturnType<T>>> {
+	const debounced = debounce((resolve, reject, args) => {
+		func(...args)
+			.then(resolve)
+			.catch(reject);
+	}, wait);
+
+	return (...args: Parameters<T>) => {
+		return new Promise((resolve, reject) => {
+			debounced(resolve, reject, args);
+		});
+	};
+}
