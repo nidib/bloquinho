ace6998e44734b18d854e9f70c1a0a19fd25d94a
Richard Bidin <richardbidin@outlook.com>
2025-02-08 03:16:39 -0300

    feat(app): type package for bloquinho

---
+++ app/src/lib/types/bloquinho.ts
@@ -0,0 +1,25 @@
+const extensions = [
+	'txt',
+	'java',
+	'js',
+	// 'jsx',
+	'ts',
+	// 'tsx',
+	'sql',
+	'html',
+	'py',
+	'md',
+	'css',
+] as const;
+
+export type Extension = (typeof extensions)[number];
+
+export type Bloquinho = {
+	id: string;
+	title: string;
+	content: string;
+	extension: Extension;
+	last_viewed_at: Date;
+	created_at: Date;
+	updated_at: Date;
+};
