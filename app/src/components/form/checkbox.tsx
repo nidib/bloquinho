93d72b44d9bd3bce293f01db659a6d146741ad2d
Richard Bidin <richardbidin@outlook.com>
2025-02-08 03:20:05 -0300

    fix(app): animates checkbox

--- app/src/components/form/checkbox.tsx
+++ app/src/components/form/checkbox.tsx
@@ -13,10 +13,11 @@ type Props = {
 export function Checkbox(props: Props) {
 	return (
 		<CheckboxPrimitive.Root
+			id={props.id}
 			checked={props.value}
 			onCheckedChange={props.onChange}
 			className={cn(
-				'h-4 w-4 shrink-0 rounded-[4px] border border-zinc-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-700 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-700 data-[state=checked]:text-white',
+				'transition-all h-4 w-4 shrink-0 rounded-[4px] border border-zinc-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-700 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-700 data-[state=checked]:text-white',
 			)}
 		>
 			<CheckboxPrimitive.Indicator
