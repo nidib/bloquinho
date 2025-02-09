c7b652bd8132f0a963c507e87c57870ca2b323db
Richard Bidin <richardbidin@outlook.com>
2025-02-09 15:58:25 -0300

    temp(app): debugging next's env vars

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -28,6 +28,8 @@ export default async function RootLayout({ children }: Props) {
 	const underMaintenance =
 		await FeatureFlagsService.getFeatureFlagValue('UNDER_MAINTENANCE');
 
+	console.log(process.env);
+
 	return (
 		<html lang="pt-BR" className={cn(nunito.variable)}>
 			<body className="antialiased">
