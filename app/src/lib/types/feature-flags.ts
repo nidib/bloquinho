d1e301826fe2bee1e20d738fab19fb3e64519393
Richard Bidin <richardbidin@outlook.com>
2025-02-09 18:07:44 -0300

    chore(app): adds new feat. flag for the playground

--- app/src/lib/types/feature-flags.ts
+++ app/src/lib/types/feature-flags.ts
@@ -1,9 +1,11 @@
 export type FeaturesFlags = {
 	UNDER_MAINTENANCE: boolean;
+	PLAYGROUND: boolean;
 };
 
 export type Flags = keyof FeaturesFlags;
 
 export const FeatureFlagsDefaultValues: FeaturesFlags = {
 	UNDER_MAINTENANCE: false,
+	PLAYGROUND: false,
 };
