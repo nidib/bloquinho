53899e5fefd3a5b83a2cff356b8344def3ebc67f
Richard Bidin <richardbidin@outlook.com>
2025-02-11 22:38:17 -0300

    chore(app): seed the feature flags on server initialization

--- app/src/instrumentation.ts
+++ app/src/instrumentation.ts
@@ -1,4 +1,13 @@
 export async function register() {
+	// Environment variables
 	const { Envs } = await import('src/utils/constants/envs');
 	console.info('Envs:', Envs);
+
+	if (process.env.NEXT_RUNTIME === 'nodejs') {
+		// MongoDB initialization
+		const { FeatureFlagsService } = await import(
+			'src/lib/infra/mongo/services/feature-flag-services'
+		);
+		await FeatureFlagsService.MIGRATION_ONLY.initialize();
+	}
 }
