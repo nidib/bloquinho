d695476d665ba3840c05779cfa2b1b5468702032
Richard Bidin <richardbidin@outlook.com>
2025-02-17 12:42:42 -0300

    chore(app): adds mail service

---
+++ app/src/lib/infra/mail/services/mail-services.ts
@@ -0,0 +1,49 @@
+import { type CreateEmailOptions, Resend } from 'resend';
+import {
+	type FeedbackType,
+	feedbackTypes,
+} from 'src/lib/types/support-and-feedback';
+import { Envs } from 'src/utils/constants/envs';
+import { z } from 'zod';
+
+const resend = new Resend(Envs.RESEND_API_KEY ?? 're_123');
+
+const feedbackEmailBodySchema = z.object({
+	type: z.enum(feedbackTypes),
+	message: z.string().nonempty(),
+});
+
+const titleByFeedbackType: Record<FeedbackType, string> = {
+	bug: 'Alguém encontrou um bug!',
+	feature: 'Alguém teve uma idea!',
+};
+
+async function sendFeedbackEmail(content: {
+	type: FeedbackType;
+	message: string;
+}) {
+	const body = feedbackEmailBodySchema.parse(content);
+	const email: CreateEmailOptions = {
+		from: 'Bloquinho <hello@feedback.bloquinho.app>',
+		to: ['richardbidin@outlook.com'],
+		subject: titleByFeedbackType[body.type],
+		html: body.message,
+	};
+
+	if (!Envs.RESEND_API_KEY) {
+		console.log('would send the email:', email);
+		return true;
+	}
+
+	const { error } = await resend.emails.send(email);
+	if (error) {
+		console.error({ error });
+		return false;
+	}
+
+	return true;
+}
+
+export const MailServices = {
+	sendFeedbackEmail,
+};
