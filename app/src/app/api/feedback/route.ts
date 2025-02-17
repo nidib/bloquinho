e81d8c73c0717638a343bbb6301914f94f53e3d4
Richard Bidin <richardbidin@outlook.com>
2025-02-17 12:43:45 -0300

    chore(app): adds api for mail service

---
+++ app/src/app/api/feedback/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import { MailServices } from 'src/lib/infra/mail/services/mail-services';
+
+export async function POST(request: Request) {
+	try {
+		const body = await request.json();
+		await MailServices.sendFeedbackEmail(body);
+		return NextResponse.json(null, { status: 200 });
+	} catch (error: unknown) {
+		console.error({ error });
+		return NextResponse.json(null, { status: 500 });
+	}
+}
