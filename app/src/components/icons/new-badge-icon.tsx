137c5d7e7ae90fa5ba5b7d5bd6fbda6350b25906
Richard Bidin <richardbidin@outlook.com>
2025-05-15 10:25:32 +0200

    chore: new feature badge icon component

---
+++ app/src/components/icons/new-badge-icon.tsx
@@ -0,0 +1,7 @@
+export function NewBadgeIcon() {
+	return (
+		<div className="absolute top-0 right-0 translate-x-1/2 -translate-y-1/2 bg-green-200 border border-solid border-green-500 text-green-950 py-0.5 px-1 rounded-md text-3xs select-none opacity-90">
+			NOVO
+		</div>
+	);
+}
