4a2b7a525b1bf448959dc8adec5fba969914e537
Richard Bidin <richardbidin@outlook.com>
2025-07-10 00:05:16 -0300

    chore: fixing home page text colors

--- apps/web/src/components/home-page-header/home-page-header.tsx
+++ apps/web/src/components/home-page-header/home-page-header.tsx
@@ -8,8 +8,8 @@ export function HomePageHeader() {
 
 	return (
 		<>
-			<h1 className="text-zinc-700 text-7xl font-black">{App.NAME}</h1>
-			<h2 className="text-zinc-600 text-4xl font-bold">
+			<h1 className="text-foreground/85 text-7xl font-black">{App.NAME}</h1>
+			<h2 className="text-foreground/60 text-4xl font-bold">
 				{t('AppDescription')}
 			</h2>
 		</>
